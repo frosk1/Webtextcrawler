686d262223cc666e44cc5984c2700976af1220f1
Jan Weßling <jan-ek@web.de>
2015-07-04 14:16:17 +0200

    initaler commit webtextcrawler

---
+++ webtextcrawler/spiders/kicker_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from webtextcrawler.items import KickerItem
+class KickerSpider(scrapy.Spider):
+    name = "kicker"
+    allowed_domains = ["kicker.de"]
+    start_urls = [
+        "http://www.kicker.de/news/fussball/bundesliga/spieltag/1-bundesliga/2014-15/-1/0/spieltag.html"
+    ]
+
+    #def parse(self, response):
+        # filename = response.url.split("/")[-2] + '.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+
+    def parse(self, response):
+        for href in response.selector.re('<a class="link" href="(.*)">Analyse</a>'):
+            url = response.urljoin(href)
+            print url
+            print type(url)
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('/html'):
+            item = KickerItem()
+            item['teaser'] = sel.xpath('//*[@id="ovArtikel"]/p[1]/text()').extract()
+            yield item
